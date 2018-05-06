d3a10b5385b9df50b6e1e42de14c5da8f51bf678
Denis Andreev <dnandreev@hotmail.com>
2018-05-06 23:55:11 +0300

    #68 Fix path to SqlPackage

--- Deployment/DataRoom-DB-Deploy-test.ps1
+++ Deployment/DataRoom-DB-Deploy-test.ps1
@@ -1,5 +1,9 @@
 param ($config = "Debug", $SqlPackageVer = 140)
 
-$SqlPackageExe = "C:\Program Files (x86)\Microsoft SQL Server\$SqlPackageVer\DAC\bin\SqlPackage.exe"
+$SqlPackageExe = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\$SqlPackageVer\SqlPackage.exe"
+if (-not (Test-Path $SqlPackageExe)) 
+{
+	$SqlPackageExe = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\$SqlPackageVer\SqlPackage.exe"
+}
 
 & $SqlPackageExe /a:Publish /sf:"..\Src\FileStorage.DB.SqlServer\bin\$config\FileStorage.DB.SqlServer.dacpac" /pr:"..\Src\FileStorage.DB.SqlServer\FileStorage.DB.SqlServer.publish.xml"
