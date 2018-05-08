478986543be9e1c88a098cf00f114ba617a4fb9f
Denis Andreev <dnandreev@hotmail.com>
2018-05-08 16:35:35 +0300

    Add Web service project

---
+++ Src/FileStorage.WebService/Controllers/FileStorageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileStorage.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileStorage.WebService.Controllers
+{
+    [Route("api/[controller]")]
+    public class FileStorageController : Controller
+    {
+        private readonly IFileStorageService _fileStorageService;
+
+        public FileStorageController(IFileStorageService fileStorageService)
+        {
+            _fileStorageService = fileStorageService;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+    }
+}
---
+++ Src/FileStorage.WebService/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Repository;
+using FileStorage.BL;
+using FileStorage.Core;
+using FileStorage.Repository.Core;
+using FileStorage.Repository.EF;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace FileStorage.WebService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IDbConfig, FileStorageConfig>();
+            services.AddTransient<IFileStorageUnitOfWork, FileStorageUnitOfWork>();
+            services.AddTransient<IFileStorageConfig, FileStorageConfig>();
+            services.AddTransient<IFileStorageService, FileStorageService>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
